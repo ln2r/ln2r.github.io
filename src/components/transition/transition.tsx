031c73988f0a81d2bd091b356b3a8a7ea7cb756c
ln2r <scorch_62@live.com>
2024-02-10 21:39:54 +0700

    feat(site): add reduced motion

--- src/components/transition/transition.tsx
+++ src/components/transition/transition.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useReducedMotion } from "framer-motion";
 
 export default function Transition({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const reducedMotion = useReducedMotion();
+
   return (
     <motion.div
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: "easeInOut", duration: 0.75 }}
+      transition={{ ease: "easeInOut", duration: reducedMotion? 0 : 0.75 }}
     >
       {children}
     </motion.div>
