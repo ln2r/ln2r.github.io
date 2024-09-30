6adf4356ec84152eae97cd45d78e608bf68369bb
ln2r <scorch_62@live.com>
2024-09-30 15:20:10 +0700

    feat(data): add contacts

---
+++ src/lib/contacts.tsx
@@ -0,0 +1,10 @@
+export const contacts = [
+  {
+    "platform": "discord",
+    "url": "https://discord.com/users/132712988875423745",
+  },
+  {
+    "platform": "github",
+    "url": "https://github.com/ln2r",
+  },
+]
