b50ecd342d033fdac0bca5c48177f4f0256f33c8
ln2r <scorch_62@live.com>
2025-10-08 10:18:18 +0700

    feat - initial change to svelte

--- src/lib/utils/random.ts
+++ src/lib/utils/random.ts
@@ -4,21 +4,21 @@ export enum RandomType {
   MIXED = 'mixed',
 }
 
-export const getRandom = (length: number, type: RandomType) => {
+export const random = (length: number, type: RandomType) => {
   let result = '';
   let counter = 0;
-  
 
-  const characters = type === RandomType.LETTERS 
-    ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : type === RandomType.NUMBER 
-    ? '0123456789' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+  const characters = type === RandomType.LETTERS
+    ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : type === RandomType.NUMBER
+      ? '0123456789' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
   const charactersLength = characters.length;
-  
+
   while (counter < length) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
     counter += 1;
   }
-  
+
   return result;
 }
