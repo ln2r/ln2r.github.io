3743f9c1dac528b53d429d8eeca8491163de1312
ln2r <scorch_62@live.com>
2025-10-26 12:51:49 +0700

    feat - add initial blog route

---
+++ src/routes/blog/+layout.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	// import favicon from '$lib/assets/favicon.svg';
+    import favicon from '$lib/assets/origami-blank.png';
+    import '@fontsource-variable/jetbrains-mono';
+    import '@fontsource/redacted';
+    import Menu from '$lib/components/menu.svelte';
+    import {random, RandomType} from "$lib/utils/random";
+
+	let { children } = $props();
+</script>
+
+<style>
+    h3, p, a {
+        padding: 0;
+        margin: 0;
+    }
+
+    :global(body) {
+        font-family: "JetBrains Mono", monospace;
+        font-size: medium;
+
+        background: black;
+        color: white;
+
+        a {
+            color: white;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: cornflowerblue;
+        }
+    }
+
+    footer {
+        position: fixed;
+        font-weight: 100;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        text-align: right;
+        margin: 0 4em 4em 0;
+        color: darkgrey;
+
+        @media (min-width: 950px){
+            z-index: -10;
+        }
+    }
+</style>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
+
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<style>
+    .content {
+        width: 45em;
+        margin: 4em auto 0 auto;
+
+        @media only screen and (max-width: 600px) {
+            width: 100%;
+        }
+    }
+
+    div {
+        p {
+            text-align: justify;
+            margin: 0;
+        }
+    }
+</style>
+
+<div class="content">
+    <div class="title">
+        <h1>whoa hello</h1>
+    </div>
+    <div class="body">
+        <p>congrats, this is not finished yet, still working on it, maybeee someday it will be done, who knows ... yes one day, hopefully</p>
+    </div>
+</div>
