9e96239cd9e8cf33e66d546991c86cb07bb1f5bd
ln2r <scorch_62@live.com>
2025-11-09 21:21:23 +0700

    feat - update how project list mapped, add contact section

--- src/lib/utils/github.ts
+++ src/lib/utils/github.ts
@@ -2,10 +2,18 @@ export const github = async () => {
     const res = await fetch('http://localhost:8787/works');
     const items = await res.json();
 
-    return items.map((item: any) => {
-        return {
-            ...item,
-            tags: [item.language, ...item.topics]
-        }
-    });
+    const chunk = 2;
+
+    const grouped: any[] = [[], []];
+    const groupCount = Math.ceil((items.length === 0 ? 1 : items.length) / 2);
+
+    console.log(groupCount);
+
+    for (let i = 0, l = groupCount; i < l; i++) {
+        grouped[i] = items.splice(0, chunk);
+    }
+
+    console.log(grouped);
+
+    return grouped;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,32 +7,39 @@
 <style>
     .content {
         width: 45em;
-        margin: 4em auto 0 auto;
+        margin: 4em auto 4em auto;
 
         @media only screen and (max-width: 600px) {
             width: 100%;
         }
     }
 
-    .project-list, .writing-list {
+    .project-list, .writing-list, .contact-list {
         margin-top: 3em;
     }
 
-    .project-item, .writing-item {
-        margin-top: 1.5em;
-    }
-
-    .project-item, .writing-item {
+    .project-item, .writing-item, .contact-item {
         h3 {
             margin: 0;
         }
 
         p {
             margin-top: 1em;
+            text-align: left;
         }
 
         small {
             color: cornflowerblue;
+        }
+    }
+
+    .project-row {
+        width: auto;
+        display: flex;
+
+        .project-item {
+            width: 50%;
+            padding: 1em;
         }
     }
 
@@ -45,21 +52,26 @@
 </style>
 
 <div class="content">
-    <p>a nerd who loves video games. mainly focused on backend development with occasional front end stuff. been active for around 7 years (4 years-ish professionally).</p>
+    <p>heavily and mainly doing backend nodejs stuff, from backend api for professional use (work) to silly random app i make to make things i do easier. outside codes i love to play games, watch movies/anime, and read books.</p>
 
     <div class="project-list">
-        <h2>projects</h2>
+        <h2>stuff i did</h2>
         {#await projects}
             {:then data}
-                {#each data as work}
-                    <div class="project-item">
-                        <h3>> {work.name}</h3>
-                        <p>{work.description ?? "no description added"}</p>
-                        <small>{work.tags.join (", ")}</small>
+                {#each data as group}
+                    <div class="project-row">
+                        {#each group as project}
+                            <div class="project-item">
+                                <h3>> <a href={project.html_url}>{project.name}</a> </h3>
+                                <p>{project.description ?? "no description added"}</p>
+                                <br />
+                                <small>{[project.language, ...project.topics].join (", ")}</small>
+                            </div>
+                        {/each}
                     </div>
                 {/each}
             {:catch error}
-            <p>unable to fetch projects data, {error}</p>
+            <p>the list seems broken... will get back to you soon.</p>
         {/await}
     </div>
 
@@ -69,6 +81,13 @@
             <h3>/ name</h3>
             <p>desc</p>
             <small>tag a, tag b, tag c, tag d</small>
+        </div>
+    </div>
+
+    <div class="contact-list">
+        <h2>contacts</h2>
+        <div class="contact-item">
+            <p>discord, github</p>
         </div>
     </div>
 </div>
