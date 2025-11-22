052d6fe3b47d8a4a789e8cca742f31310bacc0c7
ln2r <scorch_62@live.com>
2025-11-22 19:45:21 +0700

    feat - update source url

--- src/lib/utils/github.ts
+++ src/lib/utils/github.ts
@@ -1,5 +1,5 @@
 export const github = async () => {
-    const res = await fetch('http://localhost:8787/works');
+    const res = await fetch('https://mugunghwa.scorch-62.workers.dev/works');
     const items = await res.json();
 
     const chunk = 2;
@@ -7,13 +7,9 @@ export const github = async () => {
     const grouped: any[] = [[], []];
     const groupCount = Math.ceil((items.length === 0 ? 1 : items.length) / 2);
 
-    console.log(groupCount);
-
     for (let i = 0, l = groupCount; i < l; i++) {
         grouped[i] = items.splice(0, chunk);
     }
-
-    console.log(grouped);
 
     return grouped;
 }
