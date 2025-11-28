11d232584324cb8269fdf26a752ad84dc73e2d9c
ln2r <scorch_62@live.com>
2025-11-28 13:12:34 +0000

    feat - setup wrangler for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,17 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
     kit: {
         appDir: 'app',
-        adapter: adapter({
-            fallback: '404.html'
-        }),
-        paths: {
-            base: ''
-        }
+        adapter: adapter(),
+        paths: { base: '' }
     }
 };
 
