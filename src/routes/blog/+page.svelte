15fd16bbd1ee454cbed30e3845e237356abce537
ln2r <scorch_62@live.com>
2025-12-07 07:21:49 +0000

    feat - initial blog integration

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,10 @@
 <script>
 </script>
 
+<svelte:head>
+    <title>writings / ln2rworks</title>
+</svelte:head>
+
 <style>
     .content {
         width: 45em;
@@ -17,13 +21,38 @@
             margin: 0;
         }
     }
+
+    .writing-list {
+        margin-top: 3em;
+    }
+
+    .writing-item {
+        h3 {
+            margin: 0;
+        }
+
+        p {
+            margin-top: 1em;
+            text-align: left;
+        }
+
+        small {
+            color: cornflowerblue;
+        }
+    }
 </style>
 
 <div class="content">
     <div class="title">
-        <h1>whoa hello</h1>
+        <h1>writings</h1>
     </div>
     <div class="body">
-        <p>congrats, this is not finished yet, still working on it, maybeee someday it will be done, who knows ... yes one day, hopefully</p>
+        <div class="writing-list">
+            <div class="writing-item">
+                <h3>title goes here</h3>
+                <p>snippets goes here</p>
+                <small>tag a, tag b, tag c, tag d</small>
+            </div>
+        </div>
     </div>
 </div>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import SvelteMarkdown from "@humanspeak/svelte-markdown";
+
+    const title = 'post title goes here'
+    const source = `
+    # This is a header
+
+This is a paragraph with **bold** and <em>mixed HTML</em>.
+
+* List item with \`inline code\`
+* And a [link](https://svelte.dev)
+  * With nested items
+  * Supporting full markdown
+    `
+</script>
+
+<svelte:head>
+    <title>{title} / ln2rworks</title>
+</svelte:head>
+
+<style>
+    .content {
+        width: 45em;
+        margin: 4em auto 0 auto;
+
+        @media only screen and (max-width: 600px) {
+            width: 100%;
+        }
+
+        .title {
+            h1 {
+                margin: 0;
+            }
+
+            border-bottom: 1px solid darkgray;
+            padding-bottom: 1em;
+        }
+    }
+
+    div {
+        p {
+            text-align: justify;
+            margin: 0;
+        }
+    }
+</style>
+
+<div class="content">
+    <div class="title">
+        <h1>{title}</h1>
+        <small>{new Date().toDateString()}</small>
+    </div>
+
+    <SvelteMarkdown {source} />
+</div>
