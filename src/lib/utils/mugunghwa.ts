4cfdbae19d64e92c9dfc6cbd5a123ca5a307c5aa
ln2r <scorch_62@live.com>
2025-12-14 00:21:01 +0700

    feat - add get writing by slug

--- src/lib/utils/mugunghwa.ts
+++ src/lib/utils/mugunghwa.ts
@@ -2,23 +2,43 @@ export class Mugunghwa {
     constructor(private readonly fetchFn: typeof fetch = fetch) {}
 
     async works() {
-        const res = await this.fetchFn('http://localhost:8787/works');
-        const items = await res.json();
+        try {
+            const res = await this.fetchFn('http://localhost:8787/works');
+            const items = await res.json();
 
-        const chunk = 2;
+            const chunk = 2;
 
-        const grouped: any[] = [[], []];
-        const groupCount = Math.ceil((items.length === 0 ? 1 : items.length) / 2);
+            const grouped: any[] = [[], []];
+            const groupCount = Math.ceil((items.length === 0 ? 1 : items.length) / 2);
 
-        for (let i = 0, l = groupCount; i < l; i++) {
-            grouped[i] = items.splice(0, chunk);
+            for (let i = 0, l = groupCount; i < l; i++) {
+                grouped[i] = items.splice(0, chunk);
+            }
+
+            return grouped;
+        } catch (error) {
+            console.error(error);
+            return error;
         }
-
-        return grouped;
     }
 
     async writings() {
-        const res = await this.fetchFn('http://localhost:8787/writings');
-        return await res.json();
+        try {
+            const res = await this.fetchFn('http://localhost:8787/writings');
+            return await res.json();
+        } catch (error) {
+            console.error(error);
+            return error;
+        }
+    }
+
+    async writing(slug: string) {
+        try {
+            const res = await this.fetchFn('http://localhost:8787/writing/' + slug);
+            return await res.json();
+        } catch (error) {
+            console.error(error);
+            return error;
+        }
     }
 }
