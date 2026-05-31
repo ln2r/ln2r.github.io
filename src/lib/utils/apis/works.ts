39b310938dff845bf1d9eabb6f3e220eccf5c7b3
ln2r <scorch_62@live.com>
2026-05-31 14:03:12 +0000

    feat: add crud endpoints to works

--- src/lib/utils/apis/works.ts
+++ src/lib/utils/apis/works.ts
@@ -1,8 +1,23 @@
 import { env } from "$env/dynamic/public";
+import type { Session } from "../session";
 const { PUBLIC_API_BASE } = env;
 
+export interface WorkAdd {
+    title: String;
+    description: String;
+    url?: String | null;
+    stacks: String;
+}
+
+export interface WorkUpdate extends WorkAdd {
+    id: String;
+}
+
 export class Work {
-    constructor(private readonly fetchFn: typeof fetch = fetch) {}
+    constructor(
+        private readonly fetchFn: typeof fetch = fetch,
+        private readonly session?: Session,
+    ) {}
 
     async list() {
         try {
@@ -13,6 +28,50 @@ export class Work {
         } catch (error) {
             console.error(error);
             return error;
+        }
+    }
+
+    async getOneById(id: string) {
+        try {
+            const res = await this.fetchFn(`${PUBLIC_API_BASE}/works/${id}`, {
+                cache: "no-store",
+            });
+            const body = await res.json();
+
+            return body.data;
+        } catch (err) {
+            console.error(err);
+            return err;
+        }
+    }
+
+    async add(body: WorkAdd) {
+        try {
+            const res = await this.session?.process(
+                `${PUBLIC_API_BASE}/works`,
+                "POST",
+                body,
+            );
+
+            return res;
+        } catch (err) {
+            console.error(err);
+            return err;
+        }
+    }
+
+    async update(id: string, body: WorkUpdate) {
+        try {
+            const res = await this.session?.process(
+                `${PUBLIC_API_BASE}/works/${id}`,
+                "PATCH",
+                body,
+            );
+
+            return res;
+        } catch (err) {
+            console.error(err);
+            return err;
         }
     }
 }
